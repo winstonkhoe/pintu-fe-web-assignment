adb381cd29a3134794313041315cbc3ff7792694
Winston <winstonkhoe@yahoo.com>
2024-11-22 00:11:34 +0700

    refactor: remove unused import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Providers from '@/providers';
 
 const geistSans = localFont({
